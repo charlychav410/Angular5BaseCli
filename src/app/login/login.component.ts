1a59ee04865146a1de7678cbb0615daeb38ec6da
charlychav410 <charlychav410@gmail.com>
2018-04-20 17:39:07 -0500

    final

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -17,17 +17,22 @@ export class LoginComponent implements OnInit{
 user:string;
 password:string;
 private validar;
-constructor (private _loginService:LoginService, private _router:Router){
+constructor (private _loginService:LoginService, private _router:Router){}
+
+ngOnInit(){}
+
+loginbutton(user1:string, password1:string){
+   
+    this.validar = this._loginService.evaluaUser(this.user=user1, this.password=password1);    
+        if(this.validar ==='true'){
+            this._router.navigate(['']);
+        }
+        else{
+            alert('Datos equivocados');
+                   
+            //this.user='';
+            //this.password='';
+        }
+    }
 
 }
-
-ngOnInit(){
-
-}
-loginbutton(){
-this.validar = this._loginService.evaluaUser(this.user,this.password);    
-if(this.validar ==='true'){this._router.navigate(['']);}
-else{alert('Datos equivocados'); 
-    this.user='';
-    this.password='';}
-}}
